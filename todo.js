c244e64435bc7cc20ad99f6fd9cf083deb3ab677
robninxraynn <24@felix-au.me>
2026-08-02 03:55:11 +0530

    Update todo.js - Step 7

--- todo.js
+++ todo.js
@@ -1,3 +1,7 @@
 const list = ['Code', 'Test', 'Ship'];
 const ul = document.getElementById('tasks');
 if(ul) { list.forEach(t => {
+    let li = document.createElement('li');
+    li.innerText = t;
+    ul.appendChild(li);
+});}
